5dd0939ed30bd6698db3286193ab22f9e8c98149
I-Tkachenko <54814782+I-Tkachenko@users.noreply.github.com>
2019-12-07 15:00:35 +0200

    -

--- js/sliders.js
+++ js/sliders.js
@@ -7,13 +7,4 @@ $(document).ready(function () {
         nextArrow: '<button class="next"><img src="img/icons/next.png" alt=""></button>'
 
     });
-// gallery_slider
-    $('.images_slider').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<button class="prev_for_gallery"><img src="img/icons/prev.png" alt=""></button>',
-        nextArrow: '<button class="next_for_gallery"><img src="img/icons/next.png" alt=""></button>'
-
-    });
 });
